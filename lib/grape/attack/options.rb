b8a6fa1382f534332ee04b1ba8f1fdad5e451822
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2016-05-14 17:35:13 +0200

    Allows to pass proc to options. Fixes #4 #10

--- lib/grape/attack/options.rb
+++ lib/grape/attack/options.rb
@@ -4,14 +4,36 @@ module Grape
   module Attack
     class Options
       include ActiveModel::Model
+      include ActiveModel::Validations
 
       attr_accessor :max, :per, :identifier, :remaining
 
-      validates :max, presence: true
-      validates :per, presence: true
+      class ProcOrNumberValidator < ActiveModel::EachValidator
+        def validate_each(record, attribute, value)
+          return true if value.is_a?(Numeric)
+          return true if value.is_a?(Proc) && value.call.is_a?(Numeric)
+
+          record.errors.add attribute, "must be either a proc resolving in a numeric or a numeric"
+        end
+      end
+
+      validates :max, proc_or_number: true
+      validates :per, proc_or_number: true
 
       def identifier
         @identifier || Proc.new {}
+      end
+
+      def max
+        return @max if @max.is_a?(Numeric)
+        return @max.call if @max.is_a?(Proc)
+        super
+      end
+
+      def per
+        return @per if @per.is_a?(Numeric)
+        return @per.call if @per.is_a?(Proc)
+        super
       end
 
     end
